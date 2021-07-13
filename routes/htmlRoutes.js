1d213f6e4995569f8a6c3cebfd096deafb7923dc
DRockmore89 <donaldrockmore@yahoo.com>
2021-07-13 02:18:38 -0400

    commit

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -13,7 +13,7 @@ Router.get("/stats", (req, res) =>
  res.sendFile(path.join(__dirname, "../public/stats.html"))
 );
 
-router.get('/', (req, res) => {
+Router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
